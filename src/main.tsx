05af502b8c8f8b9871939b7e52ea04eb1b3e03e6
JoneHyttinen <jone.hyttinen@myy.haaga-helia.fi>
2026-09-07 11:53:28 +0300

    feat: implement authentication and protected routes in AppRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
-import AppRouter from "./Router/AppRouter.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
